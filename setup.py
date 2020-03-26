2ce85073267ae6c4e2b660cbf4f9190e5875f8f5
Kirusi Msafiri <Kirusi.Msafiri@gmail.com>
2020-03-26 15:31:20 -0400

    Fixed license name

--- setup.py
+++ setup.py
@@ -4,29 +4,25 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='hook-run',
-    version='1.0.0',
-    author='Kirusi Msafiri',
-    author_email='kirusi.msafiri@gmail.com',
-    description='Run commands using Python virtual environment',
+    name="hook-run",
+    version="1.0.0",
+    author="Kirusi Msafiri",
+    author_email="kirusi.msafiri@gmail.com",
+    description="Run commands using Python virtual environment",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/Kirusi/hook-run',
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kirusi/hook-run",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT',
-        'Topic :: Software Development',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
     ],
-    package_dir={'': 'src'},
-    py_modules=['hookrun'],
-    python_requires='~=3.7',
-    entry_points={
-        'console_scripts': [
-            'hook-run = hookrun:run',
-        ]
-    },
+    package_dir={"": "src"},
+    py_modules=["hookrun"],
+    python_requires="~=3.7",
+    entry_points={"console_scripts": ["hook-run = hookrun:run",]},
 )
